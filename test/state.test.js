e70aa195fa79b5cab6053621869dc52bb245790e
John Rocela <jmrocela@gmail.com>
2020-07-07 17:45:03 +0800

    add preliminary test

--- test/state.test.js
+++ test/state.test.js
@@ -1,67 +1,49 @@
 const StateMachine = require('../lib/statemachine');
-//
-// module.exports.serverless = function(opts) {
-//   const definition = yaml.safeLoad(fs.readFileSync(path.resolve(process.cwd(), opts.manifest), 'utf8'));
-//   return new StateMachine(definition.stepFunctions.stateMachines, opts);
-// }
 
-const sm = new StateMachine({
-  statemachine: {
-    StartAt: 'Test',
-    States: {
-      'Test': {
-        Type: 'Task',
-        Resource: 'arn:aws:lambda:ap-southeast-1:123456789012:function:test',
-        Next: 'Map'
-      },
-      'Map': {
-        Type: 'Map',
-        Iterator: {
-          StartAt: 'Test2',
-          States: {
-            'Test2': {
-              Type: 'Task',
-              Resource: 'arn:aws:lambda:ap-southeast-1:123456789012:function:Next',
-              Next: 'Test3'
-            },
-            'Test3': {
-              Type: 'Task',
-              Resource: 'arn:aws:lambda:ap-southeast-1:123456789012:function:Next',
-              End: true
-            },
-          }
+describe('StateMachine', () => {
+  it('validates states definition', async () => {
+    expect(() => new StateMachine({ statemachine: { test } })).toThrow(
+      'data should NOT have additional properties',
+    );
+  });
+
+  it('can run steps successfully', async () => {
+    const mocked = jest.fn(() => 1);
+    const sm = new StateMachine({
+      statemachine: {
+        StartAt: 'test',
+        States: {
+          test: {
+            Type: 'Task',
+            Resource:
+              'arn:aws:lambda:ap-southeast-1:123456789012:function:test',
+            End: true,
+          },
         },
-        Next: 'Next'
       },
-      'Next': {
-        Type: 'Task',
-        Resource: 'arn:aws:lambda:ap-southeast-1:123456789012:function:next',
-        End: true
-      }
-    }
-  },
-  resolvers: {
-    Test: async (event) => ([event, event]),
-    Test2: async (event) => event,
-    Test3: async (event) => event,
-    Next: async (event) => event
-  }
+      resolvers: {
+        test: mocked,
+      },
+    });
+    const spy = jest.spyOn(sm, 'step');
+    await sm.startExecution({ nothing: true });
+    expect(spy).toHaveBeenCalled();
+    expect(mocked.mock.calls.length).toBe(1);
+  });
+
+  describe('supports Lambda Tasks', () => {});
+
+  describe('supports Tasks', () => {});
+
+  describe('supports Map', () => {});
+
+  describe('supports Parallel', () => {});
+
+  describe('supports Pass', () => {});
+
+  describe('supports Choice', () => {});
+
+  describe('supports Retry', () => {});
+
+  describe('supports Catch', () => {});
 });
-
-(async () => {
-  await sm.startLazy({ test: true });
-  sm.debug()
-  await sm.next();
-  sm.debug();
-  await sm.next();
-  sm.debug();
-  await sm.next();
-  sm.debug();
-  await sm.next();
-  sm.debug();
-  await sm.next();
-  // await sm.next();
-  // sm.debug();
-
-  // sm.report();
-})();
